7679c7ed53d936e0b420c6fa5a6c67505f5a0d59
Florka <nibilijs@gmail.com>
2026-04-13 06:26:50 -0700

    Add files via upload

---
+++ growtopia/server_data.php
@@ -0,0 +1,8 @@
+server|168.144.99.228
+port|17091
+type|1
+loginurl|gtps-loginurl-rust.vercel.app
+type2|1
+#maint|`5The server has on maintenance mode. Please check information about the server again.
+meta|localhost
+RTENDMARKERBS1001
